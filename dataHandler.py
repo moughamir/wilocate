bb502500ba03a7e9e7ce048c3a2dd4dba0a38a6f
emilio.pinn <emilio.pinn@gmail.com>
2011-01-29 15:12:50 +0000

    Complete code rewrite.

---
+++ dataHandler.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import time, os, pprint
+
+def touch(files):
+  for f in files:
+    fo = open(f,'w')
+    if not fo.closed:
+      fo.close()
+
+class dataHandler:
+
+  wifi={}
+  locations={}
+
+  jsonpath=''
+
+  def __init__(self):
+    self.jsonpath=self.genPath()
+
+
+  def saveScan(self,scan,pos=None):
+
+    timestamp = int(time.time())
+
+    n=0
+    b=0
+    self.locations[timestamp]={'APs' : []}
+
+    for s in scan:
+
+      rel=False
+      try:
+	if(self.wifi[s]['location']['realiable'] > scan[s]['location']['realiable']):
+	  rel = True
+	  b+=1
+      except KeyError:
+	pass
+
+      acc=False
+      try:
+	if(self.wifi[s]['location']['accuracy'] < scan[s]['location']['accuracy']):
+	  acc = True
+	  b+=1
+      except KeyError:
+	pass
+
+      if not s in self.wifi.keys() or acc or rel:
+	self.wifi[s]=scan[s].copy()
+	n+=1
+
+      self.locations[timestamp]['APs'].append(s)
+
+    if pos:
+      self.locations[timestamp]['position'] = pos
+
+    return n,b
+
+  def jsonDump(self):
+    toret = self.getJson()
+    f = open(self.jsonpath,'w')
+    f.write(pprint.pformat(toret))
+    f.close()
+
+  def getJson(self):
+    return { 'locations' : self.locations, 'wifi' : self.wifi }
+
+  def genPath(self):
+
+    tm = time.strftime("%d-%b-%Y", time.gmtime())
+
+    dirr = 'log-' + tm
+    if not os.path.exists(dirr):
+      os.makedirs(dirr)
+
+    i=0
+    path = dirr + os.sep + str(i) + '.log'
+    while os.path.exists(path):
+      i+=1
+      path = dirr + os.sep + str(i) + '.log'
+
+    touch([ path ])
+
+    print '+ Log file:', path
+    return path
+
---
+++ locationHandler.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+import sys, httplib, json, math
+
+def addLocation(scan):
+
+  jsons={}
+  locnum=0
+
+  for a in scan:
+
+    for r in range(3):
+      
+      params = "{ \"version\": \"1.1.0\", \"host\": \"maps.google.com\", \"request_address\": \"true\", \"address_language\":\"en_GB\", \"wifi_towers\": [ { \"mac_address\": " + a.replace(':','-') + ", \"signal_strength\": 8, \"age\": 0 } ] }"
+      headers = { "Pragma" : "no-cache", "Cache-control" : "no-cache" }
+      conn = httplib.HTTPConnection("www.google.com:80")
+      try:
+	conn.request("POST", "/loc/json", params, headers)
+	response = conn.getresponse()
+      except Exception, e:
+	print '! Error querying Google about ' + a + ': (%s)' % (e.strerror) 
+	continue
+      
+      try:
+	j = json.loads(response.read())
+      except ValueError, e:
+	print '! Error decoding JSON: (%s)' % (e.strerror) 
+	continue
+      
+      if 'location' in j:
+	j = j['location'].copy()
+	if 'address' in j:
+	  locnum+=1
+	  scan[a]['location']=j
+	  break
+
+  return locnum
+
+def standard_deviation(sequence):
+  med = sum(sequence) / len(sequence)
+  variance = sum([(x-med)**2 for x in sequence]) / len(sequence)
+  return math.sqrt(variance)
+   
+def calcPosition(scan):
+  
+  tot_lat=[]
+  tot_lng=[]
+  
+  for a in scan:
+    if 'location' in scan[a]:
+      scan[a]['location']['reliable']=0 
+      if 'latitude' in scan[a]['location'] and 'longitude' in scan[a]['location']:
+	tot_lat.append(scan[a]['location']['latitude'])
+	tot_lng.append(scan[a]['location']['longitude'])
+  
+  if not (tot_lat and tot_lng):
+    return [ 0, 0 ]
+  
+  sd_lat=standard_deviation(tot_lat)*1.2
+  sd_lng=standard_deviation(tot_lng)*1.2
+  media_lat = sum(tot_lat) / len(tot_lat)
+  media_lng = sum(tot_lng) / len(tot_lng)
+  sum_lat=0
+  sum_lng=0
+  summ_num=0
+  
+  for a in scan:
+    
+    if not ('location' in scan[a] and 'latitude' in scan[a]['location'] and 'longitude' in scan[a]['location']):
+      continue
+    
+    if scan[a]['location']['latitude'] >= media_lat-sd_lat and scan[a]['location']['latitude'] <= media_lat+sd_lat and scan[a]['location']['longitude'] >= media_lng-sd_lng and scan[a]['location']['longitude'] <= media_lng+sd_lng:
+	sum_lng+=scan[a]['location']['longitude']
+	sum_lat+=scan[a]['location']['latitude']
+	summ_num+=1
+	scan[a]['location']['reliable']=1
+	
+    
+    if scan[a]['location']['accuracy'] > 22000:  
+      print 'Ho rifiutato', a, 'con', scan[a]['location']['accuracy']
+      scan[a]['location']['reliable']=0
+
+    #self.pprint(scan[a]['location'],a)
+  
+  if summ_num>0 and sum_lat>0 and sum_lng>0:
+    #print  '+ Position: http://maps.google.it/maps?q=' + str(sum_lat/summ_num) + ',' + str(sum_lng/summ_num)
+    
+    return [ sum_lat/summ_num, sum_lng/summ_num ]
+
