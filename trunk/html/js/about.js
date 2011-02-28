55492f72164156859a2a16a1dc1067c6e87f7576
marogna.enrico@gmail.com <marogna.enrico@gmail.com@db60016d-fd5a-7107-6db8-204806f28310>
2011-02-28 23:34:57 +0000

    Add about 2

---
+++ trunk/html/js/about.js
@@ -0,0 +1,5 @@
+// Popup window code
+function newPopup(url) {
+	popupWindow = window.open(
+		url,'popUpWindow','height=100px,width=100px,left=50%,top=50%,resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no,status=yes')
+}
