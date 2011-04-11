b45fa34acc537683c996371f6ba9c92cde1bbda2
emilio.pinn@gmail.com <emilio.pinn@gmail.com@db60016d-fd5a-7107-6db8-204806f28310>
2011-04-11 22:30:28 +0000

    Finally fixed 'address already in use' issue :D

--- trunk/core/httpHandler.py
+++ trunk/core/httpHandler.py
@@ -4,17 +4,20 @@ from threading import Thread, Lock
 import SimpleHTTPServer, SocketServer, os, sys, urllib2, socket, mimetypes, time
 try: import json
 except ImportError: import simplejson as json
+try:
+  import wx
+  #import wx.lib.newevent
+except ImportError:
+  print '! Install wxPython library version 2.6 with \'sudo apt-get install python-wxgtk2.6\''
+  sys.exit(1)
 
-http_running = (False, 'Not Running', False)
 
+#WebStateUpdateEvent, WEB_STATE_EVENT = wx.lib.newevent.NewEvent()
 
 data = None
 
-class StoppableHttpServer (SocketServer.TCPServer):
-    def serve_forever (self):
-	global http_running
-        while http_running[0] and not http_running[2]:
-            self.handle_request()
+class ExitableSocketServer(SocketServer.TCPServer):
+  allow_reuse_address = True
 
 class httpRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
@@ -61,6 +64,11 @@ class httpRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 class httpHandler ( Thread ):
 
    port = -1
+   
+   running = False
+   forced_quit = False
+   
+   httpd = None
 
    def __init__(self, parentApp, jsondata, port=8000):
 
@@ -68,58 +76,49 @@ class httpHandler ( Thread ):
      data=jsondata
      self.port=port
      self.parent=parentApp
-
      Thread.__init__ ( self )
 
    def stop(self):
+     self.__changeState(False, 'Web interface stopped')
+     self.forced_quit=True
+     if self.httpd:
+	self.httpd.shutdown()
 
-     global http_running
+     #for n in range(3):
+	#try:
+	    #urllib2.urlopen('http://localhost:' + str(self.port) + '/', timeout=1)
+	#except Exception, e:
+	  #pass
+	#else:
+	  #break
 
-     self.changeState(False, 'Web interface stopped',True)
+   def __changeState(self, state, msg, displayerror = False):
+      self.running = state
+      #wx.PostEvent(self.parent, WebStateUpdateEvent())
+      #wx.PostEvent(target, SomeNewEvent(attr1=foo, attr2=bar))
+      self.parent.WebState(state,msg,displayerror)
 
-     for n in range(3):
+   def isRunning(self):
+     return self.running
+
+   def run ( self ):
+
+      while True:
+	
 	try:
-	    urllib2.urlopen('http://localhost:' + str(self.port) + '/', timeout=1)
+	  self.httpd = ExitableSocketServer(('127.0.0.1', self.port), httpRequestHandler)
 	except Exception, e:
-	  pass
+	  print '!', e
+	  self.__changeState(False, 'Port ' + str(self.port) + ' unavailable,\nplease shutdown any other process that keep port open.\nRetry in 10s.',True)
+	  time.sleep(10)
 	else:
 	  break
 
-   def changeState(self,state, msg, forced_quit):
-      global http_running
-      http_running=(state,msg,forced_quit)
-      self.parent.WebStarted()
-
-   def isRunning(self):
-     global http_running
-     return http_running
-
-   def run ( self ):
-      global http_running
-
-      httpd=None
-
-      while True:
-	if not http_running[0]:
-	  try:
-	    httpd = StoppableHttpServer(('127.0.0.1', self.port), httpRequestHandler)
-	    httpd.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	  except Exception, e:
-	    print '!', e
-	    self.changeState(False, 'Port ' + str(self.port) + ' busy,\nretrying in 5s',False)
-	    time.sleep(5)
-	  else:
-	    break
-
-	if http_running[2]:
-
+	if self.forced_quit:
 	  break
 
+      if self.httpd:
+	self.__changeState(True, "Running on port " + str(self.port))
+	self.httpd.serve_forever()
 
-      if httpd:
-	self.changeState(True, "Running on port " + str(self.port),False)
-	sa = httpd.socket.getsockname()
-	httpd.serve_forever()
-	httpd.socket.close()
-
-      self.changeState(False, 'Web interface stopped', True)
+      self.__changeState(False, 'Web interface stopped')
